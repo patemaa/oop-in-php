c7742cc9b6948891e129c04b6c0cbdca45d1bec9
Fatma R <anthoschichequs@gmail.com>
2025-01-15 16:37:45 +0300

    .gitignore dosyasi olusturuldu, index.php ve ep1.php dosyalari eklendi.

---
+++ ep1.php
@@ -0,0 +1,25 @@
+<?php
+
+class Post
+{
+    public function archive()
+    {
+
+    }
+    public function like()
+    {
+
+    }
+    public function share()
+    {
+
+    }
+
+    //Class defines the overall structure and behaviour for a concept in our codebase.
+    //Classes => blueprints
+    //Objects => implementations
+    //Class names should start with capital letters.
+    //There's no rule saying that the name of the class have to be a noun, but in general we use nouns.
+    //For methods, we are looking for verbs.
+    //When we are giving a name to the classes/methods, even its embarrassing we should say it loud so we can decide the best name for that class/method.
+}
